cbb0c2965368994fc206cf5fdb9974ccdf8f9548
BaptisteThomann <67266070+BaptisteThomann@users.noreply.github.com>
2020-07-17 10:01:01 +0200

    MyDevBlog

--- articles.php
+++ articles.php
@@ -4,7 +4,6 @@
 
 <link rel="stylesheet" type="text/css" href="index.css" media="screen" />
 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
 
